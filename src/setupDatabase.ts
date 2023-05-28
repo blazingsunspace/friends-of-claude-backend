2fa3f4d5a3c49732b04c0bbcc8f97cb33ccafadd
Milorad Đuković <donmilorad87@gmail.com>
2023-05-28 02:01:50 +0200

    setup project

---
+++ src/setupDatabase.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+
+import Logger from 'bunyan'
+import { config } from './config'
+
+const log: Logger = config.createLogger('database')
+
+export default function () {
+	const connect = function () {
+		mongoose
+			.connect(`${config.DATABASE_URL}`)
+			.then(function () {
+				log.info('success connected to mongo db')
+			})
+			.catch(function (error) {
+				log.error('error connecting to database', error)
+				return process.exit(1)
+			})
+	}
+	connect()
+
+	mongoose.connection.on('disconected', connect)
+}
+
+/* import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient() */
+/* async function main() {
+    // ... you will write your Prisma Client queries here
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    }) */
